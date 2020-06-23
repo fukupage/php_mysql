ff579d8d5b56889b87fd341935ac18326befd1c5
fukupage <4033990+fukupage@users.noreply.github.com>
2020-06-23 14:21:33 +0900

    昨日の不具合を修正

--- bbs/dbconnect.php
+++ bbs/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
-  try{
-    $db = new PDO('mysql:dbname=mini_bbs;host=localhost:8889;charset=utf8', 'root', 'root' );
-  } catch (PDOException $e){
-    echo 'DB接続エラー：' . $e -> getMessage();
-  }
+try{
+  $db = new PDO('mysql:dbname=mini_bbs;host=localhost:8889;charset=utf8','root','root');
+} catch(PDOException $e){
+  print('データベース接続エラー：' . $e ->getMessage());
+}
 ?>
--- bbs/join/index.php
+++ bbs/join/index.php
@@ -3,34 +3,18 @@ session_start();
 require('../dbconnect.php');
 
 if(!empty($_POST)){
-	if ($_POST['name'] === '') {
+	if($_POST['name'] === ''){
 		$error['name'] = 'blank';
 	}
-
-	if ($_POST['email'] === '') {
+	if($_POST['email'] === ''){
 		$error['email'] = 'blank';
 	}
-
-	if (strlen($_POST['password']) < 4) {
+	if(strlen($_POST['password']) < 4){
 		$error['password'] = 'length';
-
 	}
-	if ($_POST['password'] === '') {
+	if($_POST['password'] === ''){
 		$error['password'] = 'blank';
 	}
-	var_dump($error);
-/*
-	//アカウントの重複チェック
-	if(!empty($error)){
-		$member = $db -> prepare('SELECT COUNT(*) AS cnt FROM members WHERE email=?');
-		$member -> execute(array($_POST['email']));
-		$record = $member -> fetch();
-		if($record['cnt'] > 0){
-			$error['email'] = 'duplicate';
-			echo $error;
-		}
-	}
-*/
 	$fileName = $_FILES['image']['name'];
 	if(!empty($fileName)){
 		$ext = substr($fileName, -3);
@@ -38,25 +22,42 @@ if(!empty($_POST)){
 			$error['image'] = 'type';
 		}
 	}
+	//重複チェック　duplicate
+	if(empty($error)){
+		$member = $db -> prepare('SELECT COUNT(*) AS cnt FROM members WHERE email=?');
+		$member ->execute(array($_POST['email']));
+		$record = $member -> fetch();
+		if($record['cnt'] > 0){
+			$error['email'] = 'duplicate';
+		}
+	}
+
+
 
 	if(empty($error)){
 		$image = date('YmdHis') . $_FILES['image']['name'];
-		move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' .$image );
+		move_uploaded_file($_FILES['image']['tmp_name'], '../member_picture/' . $image);
 		$_SESSION['join'] = $_POST;
 		$_SESSION['join']['image'] = $image;
 		header('Location: check.php');
 		exit();
 	}
+	// if(empty($error)){
+	// 	$_SESSION['join'] = $_POST;
+	// 	header('Location: check.php');
+	// 	exit();
+	// }
 }
 
-if($_REQUEST['action'] == 'rewrite' && isset($_SETTION['join'])){
+if($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])){
 	$_POST = $_SESSION['join'];
 }
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -65,58 +66,59 @@ if($_REQUEST['action'] == 'rewrite' && isset($_SETTION['join'])){
 
 	<link rel="stylesheet" href="../style.css" />
 </head>
-
 <body>
-	<div id="wrap">
-		<div id="head">
-			<h1>会員登録</h1>
-		</div>
+<div id="wrap">
+<div id="head">
+<h1>会員登録</h1>
+</div>
 
-		<div id="content">
-			<p>次のフォームに必要事項をご記入ください。</p>
-			<form action="" method="post" enctype="multipart/form-data">
-				<dl>
-					<dt>ニックネーム<span class="required">必須</span></dt>
-					<dd>
-						<input type="text" name="name" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['name'], ENT_QUOTES)); ?>" />
-						<?php if ($error['name'] === 'blank') : ?>
-							<p class="error">＊ ニックネームを入力してください。</p>
-						<?php endif; ?>
-					</dd>
-					<dt>メールアドレス<span class="required">必須</span></dt>
-					<dd>
-						<input type="text" name="email" size="35" maxlength="255" value="<?php print(htmlspecialchars($_POST['email'], ENT_QUOTES)); ?>" />
-						<?php if ($error['email'] === 'blank') : ?>
-							<p class="error">＊ メールアドレスを入力してください。</p>
-						<?php endif; ?>
-						<?php if ($error['email'] === 'duplicate') : ?>
-							<p class="error">＊ すでに登録されているメールアドレスです。</p>
-						<?php endif; ?>
-				</dd>
-					<dt>パスワード<span class="required">必須</span></dt>
-					<dd>
-						<input type="password" name="password" size="10" maxlength="20" value="<?php print(htmlspecialchars($_POST['password'], ENT_QUOTES)); ?>" />
-						<?php if ($error['password'] === 'blank') : ?>
-							<p class="error">＊ パスワードを入力してください。</p>
-						<?php endif; ?>
-						<?php if ($error['password'] === 'length') : ?>
-							<p class="error">＊ パスワードが短すぎます（4文字以上）。</p>
-						<?php endif; ?>
-					</dd>
-					<dt>写真など</dt>
-					<dd>
-						<input type="file" name="image" size="35" value="test" />
-						<?php if ($error['image'] === 'type') : ?>
-							<p class="error">＊ 画像をアップロードしてください（jpg/gif/png）。</p>
-						<?php endif; ?>
-						<?php if(!empty($error)): ?>
-							<p class="error">＊ お手数ですが、画像を指定してください。</p>
-						<?php endif; ?>
-					</dd>
-				</dl>
-				<div><input type="submit" value="入力内容を確認する" /></div>
-			</form>
-		</div>
+<div id="content">
+<p>次のフォームに必要事項をご記入ください。</p>
+<form action="" method="post" enctype="multipart/form-data">
+	<dl>
+		<dt>ニックネーム<span class="required">必須</span></dt>
+		<dd>
+					<input type="text" name="name" size="35" maxlength="255"
+					value="<?php print(htmlspecialchars($_POST['name'],ENT_QUOTES)); ?>" />
+					<?php if($error['name'] === 'blank'): ?>
+					<p class="error">ニックネームを入力してください。</p>
+					<?php endif; ?>
+		</dd>
+		<dt>メールアドレス<span class="required">必須</span></dt>
+		<dd>
+					<input type="text" name="email" size="35" maxlength="255"
+					value="<?php print(htmlspecialchars($_POST['email'],ENT_QUOTES)); ?>" />
+					<?php if($error['email'] === 'blank'): ?>
+					<p class="error">＊ メールアドレスを入力してください。</p>
+					<?php endif; ?>
+					<?php if($error['email'] === 'duplicate'): ?>
+					<p class="error">＊ 別のメールアドレスを入力してください。</p>
+					<?php endif; ?>
+		</dd>
+		<dt>パスワード<span class="required">必須</span></dt>
+		<dd>
+					<input type="password" name="password" size="10" maxlength="20"
+					value="<?php print(htmlspecialchars($_POST['password'],ENT_QUOTES)); ?>" />
+					<?php if($error['password'] === 'blank'): ?>
+					<p class="error">＊ パスワードを入力してください。</p>
+					<?php endif; ?>
+					<?php if($error['password'] === 'length'): ?>
+					<p class="error">＊ パスワードは4文字以上設定してください。</p>
+					<?php endif; ?>
+        </dd>
+		<dt>写真など</dt>
+		<dd>
+					<input type="file" name="image" size="35" value="test" />
+					<?php if($error['image'] === 'type'): ?>
+					<p class="error">＊ 画像ファイルはjpg・gif・pngのいずれかを指定してください。</p>
+					<?php endif; ?>
+					<?php if(!empty($error)): ?>
+					<p class="error">＊ 恐れ入りますが画像を指定してください。</p>
+					<?php endif; ?>
+        </dd>
+	</dl>
+	<div><input type="submit" value="入力内容を確認する" /></div>
+</form>
+</div>
 </body>
-
 </html>
